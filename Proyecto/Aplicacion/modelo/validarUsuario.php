6bb57948452b3f44d56721f1981c1bfbf4b9e9cd
lrloachamin <43785926+lrloachamin@users.noreply.github.com>
2021-02-18 08:18:26 -0500

    Perfil Medico

--- Proyecto/Aplicacion/modelo/validarUsuario.php
+++ Proyecto/Aplicacion/modelo/validarUsuario.php
@@ -5,27 +5,37 @@ $cn=conectar();
 session_start();
     $usuario=$_POST['nombre_usuario'];
     $clave=$_POST['password_usuario'];
-    $sql="SELECT * FROM usuario WHERE usuario.NOMBREUSUARIO='$usuario' AND usuario.PASSWORDUSUARIO='$clave'";
+    $sql="SELECT * FROM usuario WHERE usuario.NOMBREUSUARIO='$usuario'";
         $res = $cn->query($sql) or die($cn->error);
         if($res->num_rows>0)
         {
-          while($row = $res->fetch_assoc()){
-                
-            $_SESSION["usuario"]=$row["NOMBREUSUARIO"];
-            $_SESSION["codigo"]=$row["CODIGOUSUARIO"];
-            if($row["TIPOUSUARIO"]=="Paciente"){
-                 header("Location: ../php/Paciente.php");
-            }
-            else if($row["TIPOUSUARIO"]=="Medico"){
-                header("Location: ../php/Medico.php");
-           }else{
-                 header("Location: ../php/Administrador.php");
-            }
-        }    
+            $sql="SELECT * FROM usuario WHERE usuario.NOMBREUSUARIO='$usuario' AND usuario.PASSWORDUSUARIO='$clave'";
+            $res = $cn->query($sql) or die($cn->error);
+            if($res->num_rows>0){
+
+                while($row = $res->fetch_assoc()){
+                    
+                $_SESSION["usuario"]=$row["NOMBREUSUARIO"];
+                $_SESSION["codigo"]=$row["CODIGOUSUARIO"];
+                if($row["TIPOUSUARIO"]=="Paciente"){
+                    header("Location: ../php/Paciente.php");
+                }
+                else if($row["TIPOUSUARIO"]=="Medico"){
+                    header("Location: ../php/Medico.php");
+                 }else{
+                    header("Location: ../php/Administrador.php");
+                }
+                }
+            }else{
+
+            echo '<script language="javascript">alert("Usuario o Clave Incorrecta");</script>';
+            echo '<script language="javascript">document.location="../index.php";</script>';
+
+            }    
         }
         else
         {
-            echo '<script language="javascript">alert("Usuario o Clave Incorrecta");</script>';
+            echo '<script language="javascript">alert("Usuario no existe");</script>';
             echo '<script language="javascript">document.location="../index.php";</script>';
             
         }
